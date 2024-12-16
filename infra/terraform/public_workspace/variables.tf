7cd53d5e3b7ceb1695fba44048119927a60f0def
omkarsardetreddemo <omkarsardetredence@gmail.com>
2024-12-16 08:18:58 -0500

    Add subscription, tenant and client vars to terraform backend

--- infra/terraform/public_workspace/variables.tf
+++ infra/terraform/public_workspace/variables.tf
@@ -1,15 +1,15 @@
-# variable "subscription_id" {
-#   type    = string
-#   default = ""
-# }
-# variable "tenant_id" {
-#   type    = string
-#   default = ""
-# }
-# variable "client_id" {
-#   type    = string
-#   default = ""
-# }
+variable "subscription_id" {
+   type    = string
+   default = ""
+}
+variable "tenant_id" {
+  type    = string
+   default = ""
+}
+variable "client_id" {
+   type    = string
+   default = ""
+ }
 
 
 ##############################
